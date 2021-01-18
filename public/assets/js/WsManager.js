b26af690d1db1730ddda964f1944ce7d06068457
Henri Larget <1727893+decima@users.noreply.github.com>
2021-01-19 00:41:41 +0100

    adding ping and connection lost behaviors (#18)
    * adding ping
    
    * Update TopazServer.go

--- public/assets/js/WsManager.js
+++ public/assets/js/WsManager.js
@@ -1,8 +1,19 @@
 export default class WsManager {
     ws
+    actions = {}
 
     constructor() {
-        this.ws = new WebSocket("ws://" + window.location.hostname + ( window.location.port ? `:${window.location.port}` : "") + "/ws")
+        this.ws = new WebSocket("ws://" + window.location.hostname + (window.location.port ? `:${window.location.port}` : "") + "/ws")
+        this.ws.onmessage = this._onEvent(this)
+    }
+
+    _onEvent(self) {
+        return (event) => {
+            let msg = JSON.parse(event.data);
+            if (self.actions[msg.type]) {
+                self.actions[msg.type](msg.data)
+            }
+        }
     }
 
     send(type, data) {
@@ -10,5 +21,9 @@ export default class WsManager {
             type,
             data
         }))
+    }
+
+    on(type, callback) {
+        this.actions[type] = callback
     }
 }
