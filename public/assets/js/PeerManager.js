8bf47a7df08030175044ba1b9900cd616da7b6f0
Benjamin <benjamin.duchadeuil@gmail.com>
2021-01-19 20:57:12 +0100

    wip

--- public/assets/js/PeerManager.js
+++ public/assets/js/PeerManager.js
@@ -9,21 +9,18 @@ export default class PeerManager {
     selfUser
     remoteUsers = {}
 
-    constructor(peer, callback) {
+    constructor(peer) {
         this.peer = peer
         this.selfUser = new PeerClient("__default")
-
-
     }
 
     async startPeering() {
         await this.startMyStream()
         this.peer.on('call', call => {
-            console.log("calling from " + call.peer)// Answer the call, providing our mediaStream
             let callType = call.metadata.type
             //todo check which callType to answer with appropriate stream
             call.answer(this.selfUser.videoStream);
-            call.on('stream', this.processReceivingMediaStream(this, call, callType));
+            call.on('stream', this.processReceivingMediaStream(call));
         });
     }
 
@@ -31,17 +28,14 @@ export default class PeerManager {
 
         if (navigator.mediaDevices.getUserMedia) {
             return await navigator.mediaDevices.getUserMedia({video: true, audio: true})
-                .then(this.getUserMediaSuccess(this))
+                .then(this.getUserMediaSuccess())
         }
         // Here I should as well implement the audio media but for now let's start simply with the video+audio single streamline.
     }
 
-
     call(peerId) {
-        console.log("calling " + peerId)
-
         let callVideo = this.peer.call(peerId, this.selfUser.videoStream, {metadata: {streamType: this.TYPE_VIDEO}});
-        callVideo.on('stream', this.processReceivingMediaStream(this, callVideo));
+        callVideo.on('stream', this.processReceivingMediaStream(callVideo));
     }
 
     closeCall(peerId) {
@@ -51,22 +45,21 @@ export default class PeerManager {
         }
     }
 
-    processReceivingMediaStream(self, call) {
+    processReceivingMediaStream(call) {
         return stream => {
-            console.log("Receiving stream from " + call.peer)
-            if (!self.remoteUsers[call.peer]) {
-                self.remoteUsers[call.peer] = new PeerClient(call.peer)
-                self.remoteUsers[call.peer].joinVideoStream(stream)
-               // self.remoteUsers[call.peer].setSound(false)
-                document.querySelector(".videos").appendChild(self.remoteUsers[call.peer].htmlElement)
+            if (!this.remoteUsers[call.peer]) {
+                this.remoteUsers[call.peer] = new PeerClient(call.peer)
+                this.remoteUsers[call.peer].joinVideoStream(stream)
+                this.remoteUsers[call.peer].setSound(false)
+                document.querySelector(".videos").appendChild(this.remoteUsers[call.peer].htmlElement)
             }
         }
     }
 
-    getUserMediaSuccess(self) {
+    getUserMediaSuccess() {
         return stream => {
-            self.selfUser.joinVideoStream(stream)
-            self.selfUser.setSound(false)
+            this.selfUser.joinVideoStream(stream)
+            this.selfUser.setSound(false)
             document.querySelector(".videos").appendChild(this.selfUser.htmlElement)
         }
     }
