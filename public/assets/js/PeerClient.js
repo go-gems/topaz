462297b2bcf1cf4467f68c0f4de38f8ce29874cf
decima <h.larget@gmail.com>
2021-01-19 01:31:45 +0100

    wip: peerClient

---
+++ public/assets/js/PeerClient.js
@@ -0,0 +1,34 @@
+export default class PeerClient {
+    peerId
+    htmlElement
+    video
+    videoStream
+
+    constructor(peerId) {
+        this.peerId = peerId
+        this._initializeHTML()
+    }
+
+    _initializeHTML() {
+        this.video = document.createElement('video');
+        this.htmlElement = document.createElement('div');
+        this.htmlElement.classList.add("video", "remote-video");
+        this.htmlElement.dataset.peerId = this.peerId;
+        this.htmlElement.appendChild(this.video);
+    }
+
+    joinVideoStream(stream) {
+        this.videoStream = stream
+        try {
+            this.video.srcObject = this.videoStream;
+        } catch (error) {
+            this.video.src = window.URL.createObjectURL(this.videoStream);
+        }
+
+        this.video.autoplay = true;
+        this.video.muted = false;
+        this.video.playsinline = true;
+    }
+
+
+}
--- public/assets/js/PeerManager.js
+++ public/assets/js/PeerManager.js
@@ -1,23 +1,69 @@
+import PeerClient from "./PeerClient.js";
+
 export default class PeerManager {
     TYPE_VIDEO = "VIDEO"
     TYPE_AUDIO = "AUDIO"
     TYPE_SCREENSHARE = "SCREENSHARE"
 
     peer
+    selfUser
+    remoteUsers = {}
 
     constructor(peer, callback) {
         this.peer = peer
+        this.selfUser = new PeerClient("__default")
+        this.waitForCall()
+        this.startMyStream().then(this.initCalls(this))
+
     }
 
-    initCalls() {
-        console.log("let's audio+video every users here")
-        // And this is where I stopped at 2 AM as I don't f**king know how to efficiently loop in object in js
-        // unless using the for Object.keys
-        call("PEERID", this.TYPE_VIDEO, null)
-        call("PEERID", this.TYPE_AUDIO, null)
+    async startMyStream() {
+        if (navigator.mediaDevices.getUserMedia) {
+            return await navigator.mediaDevices.getUserMedia({video: true, audio: true})
+                .then(this.getUserMediaSuccess(this))
+        }
+        // Here I should as well implement the audio media but for now let's start simply with the video+audio single streamline.
     }
 
-    call(peerId, type, mediastream) {
 
+    initCalls(self) {
+        return () => {
+            // And this is where I stopped at 2 AM as I don't f**king know how to efficiently loop in object in js
+            // unless using the for Object.keys
+            for (let i = 0; i < Object.keys(userList).length; i++) {
+                let user = userList[Object.keys(userList)[i]];
+                this.remoteUsers[user.peerId] = new PeerClient(user.peerId)
+                self.call(user.peerId, this.TYPE_VIDEO)
+            }
+        }
+    }
+
+    async waitForCall() {
+        console.log("waiting for call")
+        this.peer.on('call', call => {
+            console.log(call)// Answer the call, providing our mediaStream
+            call.answer(this.selfUser.videoStream);
+            call.on('stream', this.processReceivingMediaStream(this, call));
+        });
+    }
+
+    call(peerId, type) {
+        let call = this.peer.call(peerId, this.selfUser.videoStream);
+        call.on('stream', this.processReceivingMediaStream(this, call));
+    }
+
+    processReceivingMediaStream(self, call) {
+        return stream => {
+            console.log(call, stream)
+
+
+        }
+    }
+
+    getUserMediaSuccess(self) {
+        return stream => {
+            self.selfUser.joinVideoStream(stream)
+            document.querySelector(".videos").appendChild(this.selfUser.htmlElement)
+        }
     }
 }
